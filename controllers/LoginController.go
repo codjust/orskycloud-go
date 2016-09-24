d7f385a0b086928fb11e26207e00bfbf2f530012
HCW <1040385974@qq.com>
2016-09-24 23:34:30 +0800

    使用beego自带验证码库，实现验证码效果

--- controllers/LoginController.go
+++ controllers/LoginController.go
@@ -2,10 +2,23 @@ package controllers
 
 import (
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/cache"
+	"github.com/astaxie/beego/utils/captcha"
 )
 
 type LoginController struct {
 	beego.Controller
+}
+
+var cpt *captcha.Captcha
+
+func init() {
+	// use beego cache system store the captcha data
+	store := cache.NewMemoryCache()
+	cpt = captcha.NewWithFilter("/captcha/", store)
+	cpt.ChallengeNums = 4
+	cpt.StdWidth = 100
+	cpt.StdHeight = 40
 }
 
 func (c *LoginController) Login() {
