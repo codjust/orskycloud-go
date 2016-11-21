af62d3636af700dbef6e71299ae51633b2fdf9e4
huchangwei <1040385974@qq.com>
2016-11-22 00:37:54 +0800

    signup handle

--- models/redis_pool.go
+++ models/redis_pool.go
@@ -5,6 +5,7 @@ import (
 	"github.com/astaxie/beego"
 	"github.com/fzzy/radix/pool"
 	//"github.com/fzzy/radix/redis"
+	"crypto/md5"
 	"os"
 	//"strings"
 )
@@ -32,16 +33,34 @@ func init() {
 }
 
 func HandleRegist(username, password string) string {
-	// REDIS_HOST = beego.AppConfig.String("redis.host")
-	// REDIS_DB, _ = beego.AppConfig.Int("REDIS_DB")
-
-	// red, err := pool.NewPool("tcp", REDIS_HOST, 10)
-	// ErrHandlr(err)
-	//key := username + "#" + password
+	key := username + "#" + password
 	client, err := red.Get()
 	ErrHandlr(err)
-	res, err := client.Cmd("get", "name").Str()
+	res, err := client.Cmd("hget", "User", key).Str()
 	ErrHandlr(err)
+	if res == "" {
+		return "exist"
+	}
+	// srcData := []byte("iyannik0215")
+	// cipherText1 := md5.Sum(srcData)
+	// fmt.Println(cipherText1)
+
+	uid, err := client.Cmd("incr", "nextUserId").Str()
+	ErrHandlr(err)
+	srcData := []byte(uid)
+	uid = md5.Sum(srcData)
+	UserList, err := client.Cmd("get", "UserList").Str()
+	ErrHandlr(err)
+	UserList = UserList + uid + "#"
+	client.Cmd("set", "UserList", UserList)
+	client.Cmd("hset", "User", key, uid)
+	client.Cmd("incr", "UserCount")
+
+	client.Cmd("hset", "uid:"+uid, "username", username)
+	client.Cmd("hset", "uid:"+uid, "password", password)
+	localtime := time.Now().Unix()
+	client.Cmd("hset", "uid:"+uid, "sign_up_time", localtime)
+
 	red.Put(client)
 	// if res != nil {
 	// 	return res.Str()
